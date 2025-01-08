5c4d7281bf44593fc833a84d724e8bbede1422b7
FR33FA11 <dekoder.ai@pm.me>
2025-01-08 18:45:11 +0700

    Create and style form to add new projects

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { navBar } from './js/nav.js';
 import { greeting } from "./greeting.js";
 import { todo } from './js/todo.js';
 import { getDate } from './js/functions.js';
+import { Projects } from './js/Projects.js';
 // import odinImage from "./img/odin-lined.png";
 
 import homeHtml from './html/home.html';
@@ -26,9 +27,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const k = todo.item('Name', 'Description', 'Due', 'Priority', 'Notes');
   todo.addToDOM(k);
 
+  // const pageContent = document.getElementById('content');
   const projectTitle = document.getElementById('project-title');
   const mainDueDate = document.getElementById('main-due-date');
-
+  projectTitle.innerText = Projects.today.name;
+  mainDueDate.innerText = Projects.today.dueDate;
 
   // click events
   const body = document.querySelector('body');
@@ -60,15 +63,26 @@ document.addEventListener('DOMContentLoaded', function() {
             break;
 
           //
-          case 'daily-btn':
-            projectTitle.innerText = 'Daily';
-            mainDueDate.innerText = getDate();
-            break;
           case 'today-btn':
-            projectTitle.innerText = 'Today';
-            mainDueDate.innerText = getDate();
+            projectTitle.innerText = Projects.today.name;
+            mainDueDate.innerText = Projects.today.dueDate;
             break;
+          case 'daily-btn':
+            projectTitle.innerText = Projects.daily.name;
+            mainDueDate.innerText = Projects.daily.dueDate;
+            break;
+          case 'projects-btn':
+            console.log('projects-btn');
+            break;
+          case 'add-project-btn':
+            Projects.newForm();
+            break;
+          case 'close-np-form':
+            Projects.closeNewForm();
+            break;
+          case 'new-project-submit-btn':
 
+            break;
 
           default:
             if (navBar.dropdownToggle === true) {
@@ -94,6 +108,8 @@ document.addEventListener('keydown', function(event) {
   if (event.key === 'Escape') {
     if (navBar.dropdownToggle === true) {
       navBar.closeDropdownMenu();
+    } else if (Projects.toggleNewProjectForm === true) {
+      Projects.closeNewForm();
     }
   }
 });
--- src/js/Projects.js
+++ src/js/Projects.js
@@ -1,9 +1,36 @@
-const Today = {
+import { getDate, newElement } from "./functions";
 
-};
+const Projects = {
+  today: {
+    name: 'Today',
+    dueDate: getDate(),
+    todoList: []
+  },
+  daily: {
+    name: 'Daily',
+    dueDate: getDate(),
+    todoList: []
+  },
+  list: [
+    
+  ],
+  toggleNewProjectForm: false,
+  newForm() {
+    const maskPage = document.getElementById('mask-page');
+    maskPage.style.display = 'block';
+    const npForm = document.getElementById('np-form');
+    npForm.style.display = 'block';
+    document.getElementById('new-project-name').focus();
+    this.toggleNewProjectForm = true;
+  },
+  closeNewForm() {
+    document.getElementById('new-project-form').reset();
+    const maskPage = document.getElementById('mask-page');
+    maskPage.style.display = 'none';
+    const npForm = document.getElementById('np-form');
+    npForm.style.display = 'none'
+    this.toggleNewProjectForm = false;
+  }
+}
 
-const Projects = { 
-  
-};
-
-export { Projects };
+export { Projects }
